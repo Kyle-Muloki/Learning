166874f53d6b526d3838ddaec18a922c67d20def
Kyle-Muloki <kylemuloki12@gmai.com>
2023-11-08 16:42:34 +0300

    2D

---
+++ Arrays.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int i;
+	int a[5];
+
+	printf("Enter the elements of arrays: ");
+
+	for (i = 0 ; i < 5 ; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+}
---
+++ nested.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+
+void main()
+{
+	int a, i, j; 
+
+	for (j = 1 ; j <= 6 ; j++)
+	{
+		for ( ; <=30; i++)
+			printf("%d", i);
+		printf("\n");
+	}
+}
